ffb51eb43fb40c860f1b94903a949a057188132b
Akash <akashmenon11699cool@hotmail.com>
2020-04-28 14:20:39 +0530

    initial commit

---
+++ covidAPI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for covidAPI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covidAPI.settings')
+
+application = get_asgi_application()
