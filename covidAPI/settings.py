8ada3e0cdd81b60d9d6d24869bf67643c714978b
Akash <akashmenon11699cool@hotmail.com>
2020-05-02 02:16:26 +0530

    updated tasks schedule and added redis dep url

--- covidAPI/settings.py
+++ covidAPI/settings.py
@@ -102,8 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'     #os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = 'redis://localhost:6379' #os.environ['REDIS_URL']
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -113,12 +113,12 @@ from .tasks import *
 CELERY_BEAT_SCHEDULE = {
     'task1': {
         'task': 'covidAPI.tasks.t1',
-        'schedule': timedelta(minutes = 5),
+        'schedule': timedelta(hours = 2),
     },
     'task2' :{
-        'task' : 'covidAPI.tasks.t2',
-        'schedule' : timedelta(seconds = 3),
-    }
+        'task' : 'covidAPI.tasks.t3',
+        'schedule' : timedelta(hours = 2),
+    },
 }
 CELERY_TIMEZONE = 'Asia/Kolkata'
 
