9b6808079cfdeb7822515268b0b78ff42f552058
Akash <akashmenon11699cool@hotmail.com>
2020-05-02 02:14:08 +0530

    added url routes for all endpoints

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,5 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('hello/',hello),
+    path('hello/', hello),
+    path('rnaught/<str:state>/', getR0),
+    path('growth/india', getGrowthIndia),
+    path('growth/<str:state>', getGrowth),
+    path('arima/india', indiaArima),
+    path('arima/<str:state>', stateArima),
 ]
